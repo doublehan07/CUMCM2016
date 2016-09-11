d9c41ecc21119e619edc1d9be9cc2eb60aebb4d4
haldak <haldakcis@gmail.com>
2016-09-12 03:25:54 +0800

    Fix var s2 bug (missing g) of ProblemGo. Que1 & Que2 done.

--- Matlab Codes/SearchGball.m
+++ Matlab Codes/SearchGball.m
@@ -1,7 +1,7 @@
 global amin amax theta1 thetaMao;
 amin = 10000;
 amax = 0;
-for v = 1200:100:10000
+for v = 1200:100:3000
    GetGball(v);
    if (theta1 <= 5 && thetaMao <= 16)
        if v < amin amin = v; end
